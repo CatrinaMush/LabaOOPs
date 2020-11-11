2d5945dee8df27bcc537ccef0e15d19b49117a00
Mushankova <karina-mush21@mail.ru>
2020-11-11 12:24:55 +0400

    1.12 Отредактировананы все тесты класса Points

--- oop-propro/src/test/java/ru/ssau/tk/oop/propro/PointsTest.java
+++ oop-propro/src/test/java/ru/ssau/tk/oop/propro/PointsTest.java
@@ -7,14 +7,16 @@ import static ru.ssau.tk.oop.propro.Points.*;
 
 public class PointsTest extends TestCase {
 
+    static public double myError = 0.00001;
+
     public void testSum() {
         Point firstPoint = new Point(1.5, 2, 3);
         Point secondPoint = new Point(8, 9.5, 2.6);
         Point resultPoint = sum(firstPoint, secondPoint);
 
-        assertEquals(resultPoint.x, 9.5);
-        assertEquals(resultPoint.y, 11.5);
-        assertEquals(resultPoint.z, 5.6);
+        assertEquals(resultPoint.x, 9.5, myError);
+        assertEquals(resultPoint.y, 11.5, myError);
+        assertEquals(resultPoint.z, 5.6, myError);
     }
 
     public void testSubtract() {
@@ -22,9 +24,9 @@ public class PointsTest extends TestCase {
         Point secondPoint = new Point(8, 9.5, 2.6);
         Point resultPoint = subtract(firstPoint, secondPoint);
 
-        assertEquals(resultPoint.x, -6.5, 0.00001);
-        assertEquals(resultPoint.y, -7.5, 0.00001);
-        assertEquals(resultPoint.z, 0.4, 0.00001);
+        assertEquals(resultPoint.x, -6.5, myError);
+        assertEquals(resultPoint.y, -7.5, myError);
+        assertEquals(resultPoint.z, 0.4, myError);
     }
 
     public void testMultiply() {
@@ -32,9 +34,9 @@ public class PointsTest extends TestCase {
         Point secondPoint = new Point(8, 9.5, 2.6);
         Point resultPoint = multiply(firstPoint, secondPoint);
 
-        assertEquals(resultPoint.x, 12, 0.00001);
-        assertEquals(resultPoint.y, -19, 0.00001);
-        assertEquals(resultPoint.z, 0, 0.00001);
+        assertEquals(resultPoint.x, 12, myError);
+        assertEquals(resultPoint.y, -19, myError);
+        assertEquals(resultPoint.z, 0, myError);
     }
 
     public void testDivide() {
@@ -42,9 +44,9 @@ public class PointsTest extends TestCase {
         Point secondPoint = new Point(8, 3, 3);
         Point resultPoint = divide(firstPoint, secondPoint);
 
-        assertEquals(resultPoint.x, 2, 0.00001);
-        assertEquals(resultPoint.y, -0.5, 0.00001);
-        assertEquals(resultPoint.z, 1, 0.00001);
+        assertEquals(resultPoint.x, 2, myError);
+        assertEquals(resultPoint.y, -0.5, myError);
+        assertEquals(resultPoint.z, 1, myError);
 
         Point thirdPoint = new Point(0, 0, 0);
         assertThrows(ArithmeticException.class, () -> divide(firstPoint, thirdPoint));
@@ -55,30 +57,30 @@ public class PointsTest extends TestCase {
         double myFirstConstant = 5.1;
         Point resultPoint = enlarge(firstPoint, myFirstConstant);
 
-        assertEquals(resultPoint.x, 5.1, 0.00001);
-        assertEquals(resultPoint.y, -10.2, 0.00001);
-        assertEquals(resultPoint.z, 0, 0.00001);
+        assertEquals(resultPoint.x, 5.1, myError);
+        assertEquals(resultPoint.y, -10.2, myError);
+        assertEquals(resultPoint.z, 0, myError);
     }
 
     public void testLenght() {
         Point firstPoint = new Point(3, 4, 0);
-        assertEquals(lenght(firstPoint), 5, 0.00001);
+        assertEquals(lenght(firstPoint), 5, myError);
     }
 
     public void testOpposite() {
         Point firstPoint = new Point(3, 4, 0);
         Point resultPoint = opposite(firstPoint);
-        assertEquals(resultPoint.x, -3, 0.00001);
-        assertEquals(resultPoint.y, -4, 0.00001);
-        assertEquals(resultPoint.z, 0, 0.00001);
+        assertEquals(resultPoint.x, -3, myError);
+        assertEquals(resultPoint.y, -4, myError);
+        assertEquals(resultPoint.z, 0, myError);
     }
 
     public void testInverse() {
         Point firstPoint = new Point(10, 2, 5);
         Point resultPoint = inverse(firstPoint);
-        assertEquals(resultPoint.x, 0.1);
-        assertEquals(resultPoint.y, 0.5);
-        assertEquals(resultPoint.z, 0.2);
+        assertEquals(resultPoint.x, 0.1, myError);
+        assertEquals(resultPoint.y, 0.5, myError);
+        assertEquals(resultPoint.z, 0.2, myError);
 
         Point secondPoint = new Point(10, 0, 5);
         assertThrows(ArithmeticException.class, () -> inverse(secondPoint));
@@ -87,14 +89,14 @@ public class PointsTest extends TestCase {
     public void testScalarProduct() {
         Point firstVector = new Point(1, 2, 5);
         Point secondVector = new Point(2, 0, 3);
-        assertEquals(scalarProduct(firstVector, secondVector), 17, 0.0001);
+        assertEquals(scalarProduct(firstVector, secondVector), 17, myError);
     }
 
     public void testVectorProduct() {
         Point firstVector = new Point(1, 2, 5);
         Point secondVector = new Point(2, 0, 3);
-        assertEquals(vectorProduct(firstVector, secondVector).x, 6, 0.0001);
-        assertEquals(vectorProduct(firstVector, secondVector).y, 7, 0.0001);
-        assertEquals(vectorProduct(firstVector, secondVector).z, -4, 0.0001);
+        assertEquals(vectorProduct(firstVector, secondVector).x, 6, myError);
+        assertEquals(vectorProduct(firstVector, secondVector).y, 7, myError);
+        assertEquals(vectorProduct(firstVector, secondVector).z, -4, myError);
     }
 }
