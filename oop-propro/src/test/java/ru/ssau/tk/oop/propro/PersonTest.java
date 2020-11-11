a2ae00802c2e49c8d0b9b57c4463f68cce92b81d
Mushankova <karina-mush21@mail.ru>
2020-11-11 12:12:37 +0400

    1.11 Тесты для класса Person

---
+++ oop-propro/src/test/java/ru/ssau/tk/oop/propro/PersonTest.java
@@ -0,0 +1,28 @@
+package ru.ssau.tk.oop.propro;
+
+import junit.framework.TestCase;
+
+public class PersonTest extends TestCase {
+
+    Person firstPerson = new Person("Karina", "Mushankova");
+    Person secondPerson = new Person("Helena", "Shishova",25413);
+    Person thirdPerson = new Person(452623);
+
+    public void testGetFirstName() {
+        assertEquals(firstPerson.getFirstName(), "Karina");
+        assertEquals(secondPerson.getFirstName(), "Helena");
+        assertNull(thirdPerson.getFirstName());
+    }
+
+    public void testGetLastName() {
+        assertEquals(firstPerson.getLastName(), "Mushankova");
+        assertEquals(secondPerson.getLastName(), "Shishova");
+        assertNull(thirdPerson.getLastName());
+    }
+
+    public void testPassportId() {
+        assertEquals(secondPerson.getPassportId(),25413);
+        assertEquals(thirdPerson.getPassportId(), 452623);
+        assertEquals(firstPerson.getPassportId(), 0);
+    }
+}
