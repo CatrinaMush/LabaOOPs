33171b5a8848b6f2121ac8d6da49e18ce12abd20
Mushankova <karina-mush21@mail.ru>
2020-11-22 14:19:46 +0400

    2.2	Создать метод, генерирующий массив чисел по входной размерности. Массив должен быть заполнен единицами, кроме первого и последнего элемента – на их месте должны быть двойки. Метод должен возвращаться созданный массив. Покрыть метод тестами.

--- oop-propro/src/main/java/ru/ssau/tk/oop/propro/MyArrays.java
+++ oop-propro/src/main/java/ru/ssau/tk/oop/propro/MyArrays.java
@@ -5,4 +5,14 @@ public class MyArrays {
     public int[] arrayLength(int l) {
         return new int[l];
     }
+
+    public int[] nearlyOnesArray(int l) {
+        int[] myArray = new int[l];
+        for (int i = 0; i < l ; i++) {
+            myArray[i] = 1;
+        }
+        myArray[0] = 2;
+        myArray[l - 1] = 2;
+        return myArray;
+    }
 }
--- oop-propro/src/test/java/ru/ssau/tk/oop/propro/MyArraysTest.java
+++ oop-propro/src/test/java/ru/ssau/tk/oop/propro/MyArraysTest.java
@@ -1,16 +1,31 @@
 package ru.ssau.tk.oop.propro;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
-public class MyArraysTest extends TestCase {
+public class MyArraysTest {
 
     MyArrays firstArray = new MyArrays();
 
-    public void testArrayLength() {
+    @Test
+    public void arrayLength() {
         assertEquals(firstArray.arrayLength(5).length, 5);
         assertEquals(firstArray.arrayLength(10).length, 10);
         assertNotEquals(firstArray.arrayLength(3).length, 2);
+    }
+
+    @Test
+    public void nearlyOnesArray() {
+        int[] newArray = firstArray.nearlyOnesArray(5);
+        assertEquals(newArray[0], 2);
+        assertEquals(newArray[1], 1);
+        assertEquals(newArray[2], 1);
+        assertEquals(newArray[3], 1);
+        assertEquals(newArray[4], 2);
+
+        int[] veryNewArray = firstArray.nearlyOnesArray(2);
+        assertEquals(veryNewArray[0], 2);
+        assertEquals(veryNewArray[1], 2);
     }
 }
